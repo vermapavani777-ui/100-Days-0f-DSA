c78f2329df0f1206c16671a2c74ba827b4ae54e9
vermapavani777-ui <vermapavani777@gmail.com>
2026-03-23 12:51:17 +0530

    Implement polynomial representation using linked list
    This program defines a linked list structure to represent a polynomial and includes functions to insert terms and print the polynomial.

---
+++ #DSA_CODE/#DSA_CODE/Day30_Q1.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int coeff;
+    int exp;
+    struct Node* next;
+};
+
+struct Node* createNode(int c, int e) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->coeff = c;
+    newNode->exp = e;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insert(struct Node** head, int c, int e) {
+    struct Node* newNode = createNode(c, e);
+
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    struct Node* temp = *head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+}
+
+void printPolynomial(struct Node* head) {
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        if (temp->exp == 0)
+            printf("%d", temp->coeff);
+        else if (temp->exp == 1)
+            printf("%dx", temp->coeff);
+        else
+            printf("%dx^%d", temp->coeff, temp->exp);
+
+        if (temp->next != NULL)
+            printf(" + ");
+
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, coeff, exp;
+    struct Node* head = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &coeff, &exp);
+        insert(&head, coeff, exp);
+    }
+
+    printPolynomial(head);
+
+    return 0;
+}
