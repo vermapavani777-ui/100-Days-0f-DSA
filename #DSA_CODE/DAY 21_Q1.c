bf381599ef26feb3b765c01df6e55ce19299dd70
vermapavani777-ui <vermapavani777@gmail.com>
2026-03-23 12:45:52 +0530

    Implement linked list with node counting

---
+++ #DSA_CODE/DAY 21_Q1.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int countNodes(struct Node* head) {
+    int count = 0;
+    while (head != NULL) {
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        return 0;
+    }
+
+    struct Node *head = NULL, *temp = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+
+        struct Node* newNode = createNode(value);
+
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("\n%d", countNodes(head));
+
+    return 0;
+}
