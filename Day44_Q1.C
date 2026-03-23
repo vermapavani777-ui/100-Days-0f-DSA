453b39f6695eb942bd4674c935344636b6acdfd0
vermapavani777-ui <vermapavani777@gmail.com>
2026-03-23 13:03:39 +0530

    Implement binary tree with traversal methods

---
+++ Day44_Q1.C
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left, *right;
+} Node;
+
+Node* newNode(int data) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+void inorder(Node* root) {
+    if (!root) return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+void preorder(Node* root) {
+    if (!root) return;
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(Node* root) {
+    if (!root) return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    Node* nodes[n];
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = newNode(arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i]) {
+            int l = 2*i + 1;
+            int r = 2*i + 2;
+            if (l < n) nodes[i]->left = nodes[l];
+            if (r < n) nodes[i]->right = nodes[r];
+        }
+    }
+
+    Node* root = nodes[0];
+
+    inorder(root);
+    printf("\n");
+    preorder(root);
+    printf("\n");
+    postorder(root);
+
+    return 0;
+}
